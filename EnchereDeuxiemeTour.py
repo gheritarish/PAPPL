998991f42d0f07805b9f5b96e3d95098f0255dc9
gheritarish <ilissiae@gmail.com>
2019-10-22 20:47:30 +0200

    Afficher les jeux des joueurs lors des enchères

--- EnchereDeuxiemeTour.py
+++ EnchereDeuxiemeTour.py
@@ -8,6 +8,18 @@ Created on Tue Oct 22 14:45:55 2019
 def EnchereDeuxiemeTour(jeu1, jeu2, jeu3, jeu4, carte):
     """Renvoie une liste de deux éléments avec le joueur qui a pris (entre 0 et 3) et la couleur de l'atout. Renvoie une liste d'un seul élément si personne n'a pris"""
     for i in range(4):
+         if i == 0:
+             for elt in jeu1:
+                print(elt[0], elt[1], "\n")
+        elif i == 1:
+            for elt in jeu2:
+                print(elt[0], elt[1], "\n")
+        elif i == 2:
+            for elt in jeu3:
+                print(elt[0], elt[1], "\n")
+        elif i == 3:
+            for elt in jeu4:
+                print(elt[0], elt[1], "\n")
         prise = input("Voulez-vous prendre ? (y/n)")
         if prise == "y":
             couleur = carte[1]
