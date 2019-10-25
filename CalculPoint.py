686736eabf731b8befda621d8f60f7a7e70666e2
gheritarish <ilissiae@gmail.com>
2019-10-25 08:38:51 +0200

    Mise à jour du calcul des scores

--- CalculPoint.py
+++ CalculPoint.py
@@ -11,7 +11,7 @@ def calculPoint(plis_eq1, plis_eq2, gagnant_der, atout, belote):
     if plis_eq1 == []:
         points_eq2 = 252
     elif plis_eq2 == []:
-        point_eq1 = 252
+        points_eq1 = 252
     else:
         for pli in plis_eq1:
             for carte in pli:
@@ -55,10 +55,5 @@ def calculPoint(plis_eq1, plis_eq2, gagnant_der, atout, belote):
             points_eq1 += 10
         elif gagnant_der in [1, 3]:
             points_eq2 += 10
-        if belote != 4:
-            if belote in [0, 2]:
-                points_eq1 += 20
-            elif belote in [1, 3]:
-                points_eq2 += 20
     return points_eq1, points_eq2
 
