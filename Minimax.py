f3244c87344f65bd7c3df645db08552d7bd1d051
LEPELLETIER Alexandre <a.lepelletier619@laposte.net>
2019-11-22 16:41:33 +0100

    maj

--- Minimax.py
+++ Minimax.py
@@ -4,12 +4,17 @@ Created on Fri Nov 22 14:31:29 2019
 
 @author: alepe
 """
-from cartesJouables import cartesJouables
+from CartesJouables import cartesJouables
 from SuppressionCartes import suppressionCartes
+from RedistributionIA import redistributionIA
+from numpy import np
 
-def minimax(cartesIA, paquet,couleur_atout,carte_meneuse, cartes_pli):
+def minimax(cartesIA, paquet,couleur_atout,carte_meneuse, cartes_pli,difficulte):
     cartes_possibles=cartesJouables(cartesIA, cartes_pli, couleur_atout, carte_meneuse) # on recupère les cartes jouables
-    cartesAutreJoueur=suppressionCartes(suppressionCartes(paquet,cartes_pli), cartesIA)
+    cartesAutreJoueur=suppressionCartes(suppressionCartes(paquet,cartes_pli), cartesIA) #cartes à redistribuer
+    poids=np.zeros(len(cartes_possibles)) #matrice des poids de chaque cartes
+    for i in range(difficulte):
+        jeu1,jeu2,jeu3=redistributionIA(cartesAutreJoueur,len(cartes_pli))
     
     
     
