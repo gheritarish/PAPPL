6eeb01a9585b5a61292fc9bfa4a446c69c161509
LEPELLETIER Alexandre <a.lepelletier619@laposte.net>
2019-12-15 14:53:24 +0100

    changement num_joueur dans pli

--- src_minimax_interface_graph/Pli.py
+++ src_minimax_interface_graph/Pli.py
@@ -32,50 +32,50 @@ def pli(jeu1, jeu2, jeu3, jeu4, gagnant_prec, belote, rebelote, couleur_atout, j
                 jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliHumain(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, num_pli, (3-gagnant_prec)%4,jo,(gagnant-gagnant_prec)%4)
         elif joueur[(gagnant_prec + joues) % 4][1] == "IAaleatoire": # Si le joueur est une IA aleatoire
             if (gagnant_prec + joues) % 4 == 0:
-                jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIA(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, num_pli, (0-gagnant_prec)%4)
+                jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIA(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, num_pli, 0)
             elif (gagnant_prec + joues) % 4 == 1:
-                jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIA(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, num_pli, (1-gagnant_prec)%4)
+                jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIA(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, num_pli, 1)
             elif (gagnant_prec + joues) % 4 == 2:
-                jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIA(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, num_pli, (2-gagnant_prec)%4)
+                jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIA(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, num_pli, 2)
             elif (gagnant_prec + joues) % 4 == 3:
-                jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIA(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, num_pli, (3-gagnant_prec)%4)
+                jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIA(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, num_pli, 3)
         else :#le joueur est une IA minimax
             if (gagnant_prec + joues) % 4 == 0:
                  if len(cartes_pli)==0 :
-                     jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (0-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,preneur,plis_equipe1,plis_equipe2)
+                     jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  0,difficulte,(gagnant-gagnant_prec)%4,preneur,plis_equipe1,plis_equipe2)
                  elif len(cartes_pli)==1:
-                     jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (0-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur+1)%4,plis_equipe2,plis_equipe1)
+                     jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  0,difficulte,(gagnant-gagnant_prec)%4,(preneur+1)%4,plis_equipe2,plis_equipe1)
                  elif len(cartes_pli)==2:
-                     jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, (0-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur+2)%4,plis_equipe1,plis_equipe2)
+                     jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, 0,difficulte,(gagnant-gagnant_prec)%4,(preneur+2)%4,plis_equipe1,plis_equipe2)
                  elif len(cartes_pli)==3 :
-                     jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, (0-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur+3)%4,plis_equipe2,plis_equipe1)
+                     jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu1, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, 0,difficulte,(gagnant-gagnant_prec)%4,(preneur+3)%4,plis_equipe2,plis_equipe1)
             elif (gagnant_prec + joues) % 4 == 1:
                 if len(cartes_pli)==0 :
-                    jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (1-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur-1)%4,plis_equipe2,plis_equipe1)
+                    jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  1,difficulte,(gagnant-gagnant_prec)%4,(preneur-1)%4,plis_equipe2,plis_equipe1)
                 elif len(cartes_pli)==1:
-                    jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (1-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,preneur,plis_equipe1,plis_equipe2)
+                    jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  1,difficulte,(gagnant-gagnant_prec)%4,preneur,plis_equipe1,plis_equipe2)
                 elif len(cartes_pli)==2:
-                    jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (1-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur+1)%4,plis_equipe2,plis_equipe1)
+                    jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  1,difficulte,(gagnant-gagnant_prec)%4,(preneur+1)%4,plis_equipe2,plis_equipe1)
                 elif len(cartes_pli)==3:
-                    jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (1-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur+2)%4,plis_equipe1,plis_equipe2)
+                    jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu2, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  1,difficulte,(gagnant-gagnant_prec)%4,(preneur+2)%4,plis_equipe1,plis_equipe2)
             elif (gagnant_prec + joues) % 4 == 2:
                 if len(cartes_pli)==0 :
-                    jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (2-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur-2)%4,plis_equipe1,plis_equipe2)
+                    jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  2,difficulte,(gagnant-gagnant_prec)%4,(preneur-2)%4,plis_equipe1,plis_equipe2)
                 elif len(cartes_pli)==1:
-                    jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (2-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur-1)%4,plis_equipe2,plis_equipe1)
+                    jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  2,difficulte,(gagnant-gagnant_prec)%4,(preneur-1)%4,plis_equipe2,plis_equipe1)
                 elif len(cartes_pli)==2:
-                    jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (2-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,preneur,plis_equipe1,plis_equipe2)
+                    jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  2,difficulte,(gagnant-gagnant_prec)%4,preneur,plis_equipe1,plis_equipe2)
                 elif len(cartes_pli)==3:
-                    jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, (2-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur+1)%4,plis_equipe2,plis_equipe1)
+                    jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu3, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse, 2,difficulte,(gagnant-gagnant_prec)%4,(preneur+1)%4,plis_equipe2,plis_equipe1)
             elif (gagnant_prec + joues) % 4 == 3:
                 if len(cartes_pli)==0 :
-                    jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (3-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur-3)%4,plis_equipe2,plis_equipe1)           
+                    jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  3,difficulte,(gagnant-gagnant_prec)%4,(preneur-3)%4,plis_equipe2,plis_equipe1)           
                 elif len(cartes_pli)==1:
-                    jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (3-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur-2)%4,plis_equipe1,plis_equipe2)           
+                    jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  3,difficulte,(gagnant-gagnant_prec)%4,(preneur-2)%4,plis_equipe1,plis_equipe2)           
                 elif len(cartes_pli)==2:
-                    jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (3-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,(preneur-1)%4,plis_equipe2,plis_equipe1)           
+                    jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  3,difficulte,(gagnant-gagnant_prec)%4,(preneur-1)%4,plis_equipe2,plis_equipe1)           
                 elif len(cartes_pli)==3:
-                    jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  (3-gagnant_prec)%4,difficulte,(gagnant-gagnant_prec)%4,preneur,plis_equipe1,plis_equipe2)
+                    jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse = pliIAminimax(jeu4, cartes_pli, belote, rebelote, couleur_atout, carte_meneuse,  3,difficulte,(gagnant-gagnant_prec)%4,preneur,plis_equipe1,plis_equipe2)
         
         if carte_meneuse == 0: # S'il n'y a pas encore de carte meneuse, c'est la seule carte qui a été jouée
             carte_meneuse = cartes_pli[-1]
