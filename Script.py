ff27d73fb1000539ab955417f5a7733f255666d5
Baptiste Marlet <ilissiae@gmail.com>
2020-02-04 14:34:24 +0100

    Try to answer with a script

---
+++ Script.py
@@ -0,0 +1,9 @@
+import numpy
+
+def square(a):
+    return a*a
+
+def script():
+    x = np.linspace(0,5,20)
+    y = square(x)
+    return y
--- receive.py
+++ receive.py
@@ -1,9 +1,10 @@
 import socket
+from Script import script
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 20 # For fast response
-ANSWER = "I hear you, my child"
+ANSWER = script()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
