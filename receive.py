df019195dfa7966297edf1e02474f1cdc66727f3
gheritarish <ilissiae@gmail.com>
2020-02-04 14:24:44 +0100

    Change message to answer

--- receive.py
+++ receive.py
@@ -3,6 +3,7 @@ import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 20 # For fast response
+ANSWER = "I hear you, my child"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
@@ -14,5 +15,5 @@ while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
     print ("Received data:", data.decode())
-    conn.send(data)
+    conn.send(ANSWER.encode())
 conn.close()
