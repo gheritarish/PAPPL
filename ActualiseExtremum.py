49aa056f0821bc1483505c58b4bc2685b668f047
JeromeVillerot <56547309+JeromeVillerot@users.noreply.github.com>
2020-03-09 22:30:12 +0100

    changement du nom de la fonction
    changement du nom de la fonction, qui n'était pas le bon

--- ActualiseExtremum.py
+++ ActualiseExtremum.py
@@ -8,7 +8,7 @@ Created on Wed Mar  4 15:05:53 2020
 Cette fonction 
 """
 
-def actualise(position_joueur,positionIA,valeur_noeud,valeur_branche):
+def actualiseExtremum(position_joueur,positionIA,valeur_noeud,valeur_branche):
     if (valeur_noeud == None):
         return valeur_branche
     estAvecIA = ((((positionIA - position_joueur)%4)%2) == 0)
